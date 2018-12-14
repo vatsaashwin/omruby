51900b024d610e22a1a4ea479462ed1364ae78ae
vatsaashwin <aashwinvats@GMAIL.com>
2018-12-14 03:15:26 -0800

    add admin to existing user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_075544) do
+ActiveRecord::Schema.define(version: 2018_12_14_100252) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_12_14_075544) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
